0f5659322b08fab4126fc5fa466c2ca892cecc5d
Deniss Bohanovs <deniss.bohanovs@ringley.co.uk>
2014-01-27 17:03:53 +0000

    Added controler with indec page ,configured routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,47 @@
 class BooksController < ApplicationController
 	def index
-		@list_books=Books.all
+		@books=Book.find(:all)
+	end
+
+	def show
+		@book=Book.find(params[:id])
+	end
+
+	def new
+		@book =Book.new
+		@subjects =Subject.find(:all)
+	end
+
+	def create
+		@book = Book.new(params[:book])
+		if @book.save
+			redirect_to :action =>'index'
+		else
+			@subjects =Subject.find(:all)
+			render :action =>'new'
+		end
+	end
+
+	def edit
+		@book =Book.find(params[:id])
+		@subjects =Subject.find(:all)
+	end
+
+	def update
+		@book =Book.find(params[:id])
+		if @book.update_attributes(params[:book])
+			redirect_to :action =>'show', :id => @book
+		else
+			@subjects =Subject.find(:all)
+			render :action =>'edit'
+		end
+	end
+
+	def delete
+		Book.find(params[:id]).destroy
+		redirect_to :action =>'index'	
+	end
+	def show_subjects
+		@subjects =Subject.find(params[:id])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  # root :to =>"books#index"
+   map.resources :books
+   
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
