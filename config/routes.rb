7a4d00a87994d74d6d28858e4c35bddca565439d
Deniss Bohanovs <deniss.bohanovs@ringley.co.uk>
2014-02-04 16:06:06 +0000

    Root route changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  # root :to =>"books#index"
+    #root :to => 'books#index'
+   map.root :controller => :books
    map.resources :books
    
   # The priority is based upon order of creation: first created -> highest priority.
